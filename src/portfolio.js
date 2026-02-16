d2c5769d0424e4c55e84e0d04a8f4d399646d5b7
Keysie Sanchez <72845371+Keysie27@users.noreply.github.com>
2026-02-16 16:12:47 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Data & AI Engineer 🚀 with 5+ years of experience in IT, building scalable data pipelines, AI solutions, and cloud-based platforms. Skilled at transforming complex data into insights and automation for global companies."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1hPE8SgQ6Yzk4Wh6gdEREvLDKFwmR_AFI/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1XEcqtlTiYjXNTszAvbaaqXv8eO6qnMPa/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
